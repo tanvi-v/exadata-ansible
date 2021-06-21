503b5bdfa8f125b4a9f95b8f5aac1b596902dcec
Tanvi Varadhachary <tanvi.varadhachary@oracle.com>
2021-06-21 15:40:56 -0500

    restructured pdb

--- files/show_pdbs.sql
+++
@@ -1,3 +0,0 @@
-
-create pluggable database pdb3 admin user oracle IDENTIFIED BY welcome1;
-show pdbs;
---
+++ roles/pdb/files/show_pdbs.sql
@@ -0,0 +1,3 @@
+
+create pluggable database pdb4 admin user oracle IDENTIFIED BY welcome1;
+show pdbs;
